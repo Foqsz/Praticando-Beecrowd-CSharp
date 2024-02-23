e0ae85ceb749aded98289faa393efa9ec1a9aa80
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-02-23 10:26:06 -0300

    https://judge.beecrowd.com/pt/problems/view/1019

--- Beecrowd1019.cs
+++ Beecrowd1019.cs
@@ -7,7 +7,7 @@ namespace Course
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             int seg = int.Parse(Console.ReadLine());
             Console.WriteLine($"{seg / 3600}:{(seg%3600)/60}:{seg%60}");
         }
