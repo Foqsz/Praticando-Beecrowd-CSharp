bff149349725be95bbc769c815236598925ca316
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-02-23 10:11:11 -0300

    Create Beecrowd1019.cs

---
+++ Beecrowd1019.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int seg = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{seg / 3600}:{(seg%3600)/60}:{seg%60}");
+        }
+    }
+}
