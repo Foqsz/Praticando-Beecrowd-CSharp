38ff8bb87c19cce16c6d262aca8f515d08b08d81
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-03-09 11:42:31 -0300

    contar numeros impares

---
+++ Beecrowd1070.cs
@@ -0,0 +1,24 @@
+using System; 
+//by @Foqsz
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contadorImpares = 0;
+            int contador = 0;
+            int numero = int.Parse(Console.ReadLine());
+            
+            for (int i = numero; contador < 6; i++)
+            { 
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                    contador++;
+                }
+            } 
+        }
+    }
+}
+
