4716d3e3a83b6c985f871636dd1d2bbfe5ee24eb
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-03-09 10:50:49 -0300

    https://judge.beecrowd.com/pt/runs/code/38055111

---
+++ Beecrowd1059.cs
@@ -0,0 +1,16 @@
+using System; 
+// by @Foqsz
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 100; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
