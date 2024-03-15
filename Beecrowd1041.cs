316c328785ec0b4fe7826f4f2037ee37df46e6ff
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-03-15 11:19:27 -0300

    coordenadas de um ponto

---
+++ Beecrowd1041.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] coordenadas = Console.ReadLine().Split(' ');
+            float x = float.Parse(coordenadas[0]);
+            float y = float.Parse(coordenadas[1]);
+
+            if (x == 0 && y == 0)
+            {
+                Console.WriteLine("Origem");
+            }
+
+            else if (x != 0 && y == 0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+
+            else if (x == 0 && y != 0)
+            {
+                Console.WriteLine("Eixo Y");
+            }
+
+            else if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Q1");
+            }
+
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("Q2");
+            }
+
+
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Q3");
+            }
+
+            else
+            {
+                Console.WriteLine("Q4");
+            }
+        }
+    }
+}
