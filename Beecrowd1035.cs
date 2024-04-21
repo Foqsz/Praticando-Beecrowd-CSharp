16e2dd7a16dc51ce013e2a171f1c47a822599273
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-04-21 15:22:32 -0300

    https://judge.beecrowd.com/pt/runs/code/39187437

---
+++ Beecrowd1035.cs
@@ -0,0 +1,26 @@
+using System; 
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numeros = Console.ReadLine().Split(' ');
+            int a = int.Parse(numeros[0]);
+            int b = int.Parse(numeros[1]);
+            int c = int.Parse(numeros[2]);
+            int d = int.Parse(numeros[3]);
+
+            if (b > c && d > a && c + d > a + b && c > 0 && d > 0 && a % 2 == 0)
+            {
+                Console.WriteLine("Valores aceitos");
+            } 
+            else
+            {
+                Console.WriteLine("Valores nao aceitos");
+            }
+
+        }
+    }
+}
