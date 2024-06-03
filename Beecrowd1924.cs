4f5d5ef9fe8dbf46d0080069a189a4999217e1af
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-06-03 10:46:23 -0300

    citar cursos

---
+++ Beecrowd1924.cs
@@ -0,0 +1,18 @@
+using System; 
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); 
+
+            for (int i = 0; i < n; i++)
+            {
+                string nomeCurso = Console.ReadLine();
+            }
+            Console.WriteLine("Ciencia da Computacao");
+        }
+    }
+}
