b52921a58ba1ba368c357602994f99e834083dce
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-06-18 12:03:41 -0300

    https://judge.beecrowd.com/pt/runs/code/40216456

---
+++ Beecrowd2756.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("       A");
+            Console.WriteLine("      B B");
+            Console.WriteLine("     C   C");
+            Console.WriteLine("    D     D");
+            Console.WriteLine("   E       E"); 
+            Console.WriteLine("    D     D");
+            Console.WriteLine("     C   C");
+            Console.WriteLine("      B B");
+            Console.WriteLine("       A");
+        }
+    }
+}
