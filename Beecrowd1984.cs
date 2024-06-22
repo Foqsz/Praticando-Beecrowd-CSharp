a93a77a74c6eb8b7232d7c851f8beeb26a66705d
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-06-22 11:12:38 -0300

    invertendo um numero

---
+++ Beecrowd1984.cs
@@ -0,0 +1,12 @@
+using System;  
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string n = Console.ReadLine();
+
+        Console.WriteLine(n.Reverse().ToArray());   
+
+    } 
+}
