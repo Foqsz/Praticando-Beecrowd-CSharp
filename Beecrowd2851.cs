126943f911d471116d8819a480b27814100df9bf
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-07-16 11:14:52 -0300

    https://judge.beecrowd.com/pt/runs/code/40519309

---
+++ Beecrowd2851.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < N; i++)
+        {
+            string Toorg = Console.ReadLine();
+
+            Console.WriteLine("I am Toorg!");
+        }
+    }
+}
