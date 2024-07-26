9a572c9d9f2d8038b46bf69a1ac603e2bfef1161
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-07-26 11:05:45 -0300

    https://judge.beecrowd.com/pt/runs/code/40624167

---
+++ Beecrowd2780.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int D = int.Parse(Console.ReadLine());
+
+        if (D <= 800) { Console.WriteLine(1); }
+        else if (D <= 1400) { Console.WriteLine(2); }
+        else { Console.WriteLine(3); }
+    }
+
+}
