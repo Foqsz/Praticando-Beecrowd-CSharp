907302af9a8c64c1d47a463fdb10b82c11254caf
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-07-27 12:04:44 -0300

    https://judge.beecrowd.com/pt/runs/code/40633905

---
+++ Beecrowd2861.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                string s = Console.ReadLine();
+
+                Console.WriteLine("gzuz");
+            }
+        }
+    }
+}
