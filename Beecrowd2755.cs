0c3f8ff48396f1e5f5799e440f3ed74d1d346153
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-08-07 11:38:16 -0300

    Tabulação

---
+++ Beecrowd2755.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // com tabulação
+            Console.WriteLine("\"Ro'b'er\tto\\/\"");
+ 
+            Console.WriteLine("(._.) ( l: ) ( .-. ) ( :l ) (._.)");
+ 
+            Console.WriteLine("(^_-) (-_-) (-_^)");
+ 
+            Console.WriteLine("(\"_\") ('.')");
+        }
+    }
+}
