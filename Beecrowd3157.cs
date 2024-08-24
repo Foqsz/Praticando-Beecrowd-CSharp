989e1439571db40e866a80f8838123fcc09a8e96
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-08-24 12:10:44 -0300

    Create Beecrowd3157.cs

---
+++ Beecrowd3157.cs
@@ -0,0 +1,14 @@
+using System; 
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Entrada = Console.ReadLine();
+
+            Console.WriteLine("Nao se refuta ciencia com opiniao");
+        }
+    }
+}
