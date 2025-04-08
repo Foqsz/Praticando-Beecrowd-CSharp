2eda47abd35eea0d7ed56500eb5772cefac93c7d
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2025-04-08 11:46:14 -0300

    Create Beecrowd2413.cs

---
+++ AD-HOC/Beecrowd2413.cs
@@ -0,0 +1,14 @@
+using System; 
+
+namespace Course
+{
+    class ProgramBeecrowd
+    {
+        static void Main(string[] args)
+        {
+             int t = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(t * 4);
+        }
+    }
+}
